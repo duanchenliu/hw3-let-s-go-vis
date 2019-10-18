ade2f64c321268506c4126d0c0db0f6e178deefc
Duanchen Liu <liuaha@bc.edu>
2019-10-18 11:04:00 -0400

    Zoom + Tooltip Style

--- js/main.js
+++ js/main.js
@@ -1,15 +1,19 @@
 // import { csv, json } from 'd3';
 // import { feature } from 'topojson';
 
-let width = 1500,
-height = 1000;
+let width = 1000,
+height = 600;
 
 let svg = d3.select("#chart-area").append("svg")
     .attr("width", width)
-    .attr("height", height);
+    .attr("height", height)
+    .call(d3.zoom().on("zoom", function () {
+        svg.attr("transform", d3.event.transform)
+     }))
+     .append("g");
 
 let projection = d3.geoMercator()
-	.scale(175)
+	.scale(150)
     .translate([width/2, height/2]);
     
 let path = d3.geoPath()
@@ -45,14 +49,16 @@ Promise.all([
 		.attr("d", path)
         .attr('stroke', 'white')
         //bind onclick event here
-        .on("click", clicked)
+        // .on("click", clicked)
         //bind hover event here
-        .on("mouseover",mouseOverEvent);
-    
+        .on("mouseover",mouseOverEvent)
+        .on("mouseout", mouseOutEvent);
 
-
-
-
+    // svg.call(
+    //         d3.zoom().on('zoom', () => {
+    //           g.attr('transform', d3.event.transform);
+    //         })
+    //       );
     
     let node = svg.selectAll(".node")
         //we need to change this number to find the best way to present the visualization.
@@ -60,9 +66,10 @@ Promise.all([
           .enter()
           .append("circle")
           .attr("class", "node")
-          .attr("r", d => nodeSizeScale(d.population*1.5))
+          .attr("r", d => nodeSizeScale(d.population/1.5))
           .attr("fill", "gold")
           .attr("stroke", "gold")
+          .attr("opacity", 0.7)
           .attr("transform", function(d) {
              return "translate(" + projection([d.capital_long, d.capital_lat]) + ")";
            });
@@ -76,20 +83,30 @@ Promise.all([
                             .style('z-index', '10')
                             .style('color', 'black')
                                 .style('visibility', 'hidden')   
-                                .style('font-size', '20px')
+                                .style('font-size', '18px')
                             .style('font-weight', 'bold')
                             .text('')
    
    
        function clicked(d){
            console.log(d);
-           return tooltip.text("the country is:" + d.properties.name)
-                        .style("left",(d3.event.pageX)+"px")
-    					.style("top",(d3.event.pageY+20)+"px")
-                        .style("visibility", "visible")
+           
        }
       function mouseOverEvent(d){
        //    console.log("111111");
+       return tooltip.text("Country name: " + d.properties.name)
+                        .style("left",(d3.event.pageX)+"px")
+    					.style("top",(d3.event.pageY+20)+"px")
+                        .style("visibility", "visible")
+                        .style("opacity", 0.8)
+                        .style("background-color", "white")
+                        .style("border-width", "2px")
+                        .style("border-radius", "5px")
+                        .style("padding", "5px")
+      }
+
+      function mouseOutEvent(d){
+          return tooltip.style("opacity", 0);
       }
    
    
