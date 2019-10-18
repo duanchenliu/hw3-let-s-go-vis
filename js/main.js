ffb37ee64b2d3efb25bffaae32330078d6ccbcac
SpicyOwl <duanxu@bc.edu>
2019-10-18 00:55:34 -0400

    added onclick event, and the tooltip will appear on the side need decoration

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 // import { csv, json } from 'd3';
 // import { feature } from 'topojson';
 
-let width = 1000,
-height = 600;
+let width = 1500,
+height = 1000;
 
 let svg = d3.select("#chart-area").append("svg")
     .attr("width", width)
     .attr("height", height);
 
 let projection = d3.geoMercator()
-	.scale(100)
+	.scale(175)
     .translate([width/2, height/2]);
     
 let path = d3.geoPath()
@@ -21,11 +21,11 @@ Promise.all([
     d3.csv("data/country.csv"),
     d3.json("data/world-110m.json"),
 ]).then((data)=>{
-    console.log(data);
+    // console.log(data);
     let countryinfo = data[0]; //storing all the info from our dataset
     let worldmap = data[1]; //storing the map info -> to draw the map.
-    console.log(countryinfo);
-    console.log(worldmap);
+    // console.log("country info:  "+countryinfo);
+    // console.log("world map:  "+worldmap);
 
     let world = topojson.feature(worldmap, worldmap.objects.countries).features;
 
@@ -43,40 +43,54 @@ Promise.all([
 		.append("path")
 		.attr("class", "map")
 		.attr("d", path)
-		.attr('stroke', 'white');
+        .attr('stroke', 'white')
+        //bind onclick event here
+        .on("click", clicked)
+        //bind hover event here
+        .on("mouseover",mouseOverEvent);
+    
+
+
+
+
     
     let node = svg.selectAll(".node")
-          .data(countryinfo.filter(d=>d.population>10000000)) //we need to change this number to find the best way to present the visualization.
+        //we need to change this number to find the best way to present the visualization.
+          .data(countryinfo.filter(d=>d.population>10000000)) 
           .enter()
           .append("circle")
           .attr("class", "node")
-          .attr("r", d => nodeSizeScale(d.population))
+          .attr("r", d => nodeSizeScale(d.population*1.5))
           .attr("fill", "gold")
           .attr("stroke", "gold")
           .attr("transform", function(d) {
              return "translate(" + projection([d.capital_long, d.capital_lat]) + ")";
-		   });
+           });
+           
+
+
+           let tooltip = d3.select('body')
+                            .append('div')
+                            .attr("class","tooltip")
+                            .style('position', 'absolute')
+                            .style('z-index', '10')
+                            .style('color', 'black')
+                                .style('visibility', 'hidden')   
+                                .style('font-size', '20px')
+                            .style('font-weight', 'bold')
+                            .text('')
    
-    // const rowById = data.reduce((accumulator, d) => {
-    //     accumulator[d['Country code']] = d;      
-    //     return accumulator;
-    // }, {});
-
-    // const countries = topojson.feature(topoJSONdata, topoJSONdata.objects.countries);
-
-    //   countries.features.forEach(d => {
-    //     Object.assign(d.properties, rowById[+d.id]);//parse to integer
-    //   });
-
-    //   const featuresWithPopulation = countries.features
-    //   .filter(d => d.properties['2019'])
-    //   .map(d => {
-    //     d.properties['2019'] = +d.properties['2019'].replace(/ /g, '') * 1000;
-    //     return d;
-    //   });
-
-    // return {
-    //   features: countries.features,
-    //   featuresWithPopulation
-    // };
+   
+       function clicked(d){
+           console.log(d);
+           return tooltip.text("the country is:" + d.properties.name)
+                        .style("left",(d3.event.pageX)+"px")
+    					.style("top",(d3.event.pageY+20)+"px")
+                        .style("visibility", "visible")
+       }
+      function mouseOverEvent(d){
+       //    console.log("111111");
+      }
+   
+   
 });
